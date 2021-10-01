9d3e07fdc59bf265ba7621cc3b87e27ce5295102
Skippy <steelstone3@gmail.com>
2021-10-01 12:41:18 +0100

    dive stage intergration test

--- src/tests/dive_stage_should.rs
+++ src/tests/dive_stage_should.rs
@@ -1,10 +1,33 @@
 #[cfg(test)]
 mod dive_stage_should {
-    //use crate::tests::test_fixtures::test_fixtures::test_fixture_dive_stage_dive_profile_model;
+    use crate::controllers::dive_stage::dive_stage;
+    use crate::models::dive_model::dive_model::create_zhl16_dive_model;
+    use crate::tests::test_fixtures::test_fixtures::{expected_dive_profile_model, test_fixture_dive_stage_dive_profile_model, test_fixture_dive_step, test_fixture_gas_mixture};
 
     #[test]
-    #[ignore]
     fn run_dive_profile() {
-        //let actual_profile_model = test_fixture_dive_stage_dive_profile_model();
+        //Arrange
+        let zhl16 = create_zhl16_dive_model();
+        let dive_step = test_fixture_dive_step();
+        let gas_mixture = test_fixture_gas_mixture();
+        let expected_dive_profile = expected_dive_profile_model();
+        let actual_profile_model = test_fixture_dive_stage_dive_profile_model();
+
+        let result = dive_stage::run_dive_profile(zhl16, actual_profile_model, dive_step, gas_mixture);
+
+        assert_eq!(format!("{:.2}", expected_dive_profile.oxygen_at_pressure), format!("{:.2}", result.oxygen_at_pressure));
+        assert_eq!(format!("{:.2}", expected_dive_profile.helium_at_pressure), format!("{:.2}", result.helium_at_pressure));
+        assert_eq!(format!("{:.2}", expected_dive_profile.nitrogen_at_pressure), format!("{:.2}", result.nitrogen_at_pressure));
+
+        for compartment in 0..16 {
+            assert_eq!(format!("{:.1}", expected_dive_profile.tissue_pressures_nitrogen[compartment]), format!("{:.1}", result.tissue_pressures_nitrogen[compartment]));
+            assert_eq!(format!("{:.3}", expected_dive_profile.tissue_pressures_helium[compartment]), format!("{:.3}", result.tissue_pressures_helium[compartment]));
+            assert_eq!(format!("{:.2}",expected_dive_profile.tissue_pressures_total[compartment]), format!("{:.2}",result.tissue_pressures_total[compartment]));
+            assert_eq!(format!("{:.1}",expected_dive_profile.a_values[compartment]), format!("{:.1}",result.a_values[compartment]));
+            assert_eq!(format!("{:.2}",expected_dive_profile.b_values[compartment]), format!("{:.2}",result.b_values[compartment]));
+            assert_eq!(format!("{:.2}",expected_dive_profile.tolerated_ambient_pressures[compartment]), format!("{:.2}",result.tolerated_ambient_pressures[compartment]));
+            assert_eq!(format!("{:.2}",expected_dive_profile.maximum_surface_pressures[compartment]), format!("{:.2}",result.maximum_surface_pressures[compartment]));
+            assert_eq!(format!("{:.0}",expected_dive_profile.compartment_load[compartment]), format!("{:.0}",result.compartment_load[compartment]));
+        }
     }
 }
