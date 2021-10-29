febfc493731f2de14f971049b8cb293fecf3cefc
Skippy <steelstone3@gmail.com>
2021-10-29 15:36:54 +0100

    multiple cylinders

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use presenters::dive_step::dive_step::enter_dive_step;
 use crate::controllers::dive_stage::dive_stage::run_dive_profile;
 use crate::controllers::gas_management::gas_management::update_gas_management;
 use crate::factories::zhl16_dive_model::zhl16_dive_model::create_zhl16_dive_profile;
-use crate::presenters::cylinder::cylinder::{create_cylinder, display_cylinder};
+use crate::presenters::cylinders::cylinder::{create_cylinders, display_cylinder};
 use crate::presenters::dive_results::dive_results::display_results;
 use crate::presenters::presenter::presenters::write_message;
 
@@ -20,12 +20,13 @@ fn main() {
     write_message(String::from("Welcome to Bubbles Dive Planner Console Rust"));
     let dive_model = select_dive_model();
     let mut dive_profile = create_zhl16_dive_profile();
-    let cylinder = create_cylinder();
+    let cylinders = create_cylinders();
 
     loop {
         let dive_step = enter_dive_step();
-        dive_profile = run_dive_profile(dive_model, dive_profile, dive_step, cylinder.gas_mixture);
+        //select_cylinder
+        dive_profile = run_dive_profile(dive_model, dive_profile, dive_step, cylinders[0].gas_mixture);
         display_results(dive_profile);
-        display_cylinder(update_gas_management(cylinder.gas_management, dive_step));
+        display_cylinder(update_gas_management(cylinders[0].gas_management, dive_step));
     }
 }
--- src/presenters.rs
+++ src/presenters.rs
@@ -1,5 +1,5 @@
 pub mod presenter;
 pub mod dive_model;
 pub mod dive_step;
-pub mod cylinder;
+pub mod cylinders;
 pub mod dive_results;
