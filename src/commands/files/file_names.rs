9c99cb2a178e95a09d8a970a584362c8b508c35e
Skippy <steelstone3@gmail.com>
2021-11-12 12:34:24 +0000

    updated name of single source of truth

--- src/commands/files/file_names.rs
+++ src/commands/files/file_names.rs
@@ -1,4 +1,4 @@
 pub mod file_names {
-    pub const DIVE_STEP_FILE_NAME: &'static str = "dive_profile.json";
+    pub const DIVE_STEP_FILE_NAME: &'static str = "dive_step.json";
     pub const DIVE_PROFILE_FILE_NAME: &'static str = "dive_profile.json";
 }
