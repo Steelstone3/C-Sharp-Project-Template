732a48270f18830a35b3eda7538358233c32d3e6
Skippy <steelstone3@gmail.com>
2022-10-12 18:56:37 +0100

    Refactor step

--- BubblesDivePlannerTests/Models/Cylinders/GasMixtureShould.cs
+++ BubblesDivePlannerTests/Models/Cylinders/GasMixtureShould.cs
@@ -7,28 +7,20 @@ namespace BubblesDivePlannerTests.Models.Cylinders
 {
     public class GasMixtureShould
     {
-        [Theory]
-        [InlineData(21, 0, 79)]
-        [InlineData(21, 10, 69)]
-        [InlineData(100, 0, 0)]
-        [InlineData(0, 100, 0)]
-        [InlineData(101, 100, 0)]
-        [InlineData(250, 100, 0)]
-        [InlineData(20, 101, 0)]
-        [InlineData(100, 20, 0)]
-        [InlineData(20, 100, 0)]
-        [InlineData(250, 250, 0)]
-        [InlineData(100, 100, 0)]
-        [InlineData(0, 0, 100)]
-        public void ConstructAGasMixture(byte oxygen, byte helium, byte nitrogen)
+        [Fact]
+        public void ConstructAGasMixture()
         {
-            var gasMixtureBuilder = new Mock<IGasMixtureBuilder>();
-            gasMixtureBuilder.Setup(gmf => gmf.WithOxygen(oxygen));
-            gasMixtureBuilder.Setup(gmf => gmf.WithHelium(helium));
-            gasMixtureBuilder.Setup(gmf => gmf.Create()).Returns(new GasMixture(oxygen, helium, nitrogen));
-            IGasMixture gasMixture = new GasMixture(gasMixtureBuilder.Object, oxygen, helium);
+            byte oxygen = 21;
+            byte helium = 10;
 
-            gasMixtureBuilder.VerifyAll();
+            var dummyGasMixture = new Mock<IGasMixture>();
+            var stubGasMixtureBuilder = new Mock<IGasMixtureBuilder>();
+            stubGasMixtureBuilder.Setup(gmf => gmf.WithOxygen(oxygen));
+            stubGasMixtureBuilder.Setup(gmf => gmf.WithHelium(helium));
+            stubGasMixtureBuilder.Setup(gmf => gmf.Create()).Returns(dummyGasMixture.Object);
+            IGasMixture gasMixture = new GasMixture(stubGasMixtureBuilder.Object, oxygen, helium);
+
+            stubGasMixtureBuilder.VerifyAll();
         }
     }
 }
