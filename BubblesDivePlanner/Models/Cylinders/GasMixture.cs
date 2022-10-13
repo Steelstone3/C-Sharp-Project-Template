1fa1aac790c0d2436b0df4a59676daa28fb092ab
Skippy <steelstone3@gmail.com>
2022-10-13 16:23:12 +0100

    Refactor of gas mixture

--- BubblesDivePlanner/Models/Cylinders/GasMixture.cs
+++ BubblesDivePlanner/Models/Cylinders/GasMixture.cs
@@ -7,30 +7,33 @@ namespace BubblesDivePlanner.Models.Cylinders
             Oxygen = oxygen;
             Helium = helium;
 
-            CheckEdgeCases();
-            Oxygen = AssignOxygen();
-            Helium = AssignHelium();
-            CalculateNitrogen();
+            AssignGasMixture();
         }
 
         public byte Oxygen { get; private set; }
         public byte Helium { get; private set; }
         public byte Nitrogen { get; private set; }
 
+        private void AssignGasMixture()
+        {
+            switch (Oxygen)
+            {
+                case 0:
+                    Oxygen = 5;
+                    break;
+                case >= 100 when Helium >= 100:
+                    Oxygen = 100;
+                    Helium = 0;
+                    break;
+            }
+
+            Oxygen = AssignOxygen();
+            Helium = AssignHelium();
+            CalculateNitrogen();
+        }
+
         private byte AssignOxygen() => Oxygen >= 100 ? (byte)(100 - Helium) : Oxygen;
         private byte AssignHelium() => Helium >= 100 ? (byte)(100 - Oxygen) : Helium;
         private void CalculateNitrogen() => Nitrogen = (byte)(100 - Oxygen - Helium);
-        private void CheckEdgeCases()
-        {
-            if (Oxygen == 0)
-            {
-                Oxygen = 5;
-            }
-            else if (Oxygen >= 100 && Helium >= 100)
-            {
-                Oxygen = 100;
-                Helium = 0;
-            }
-        }
     }
 }
