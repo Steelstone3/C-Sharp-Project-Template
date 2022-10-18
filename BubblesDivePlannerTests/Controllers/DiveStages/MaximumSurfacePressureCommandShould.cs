219d1e99f593738b50792e01a982c6e5d2cf5982
Skippy <steelstone3@gmail.com>
2022-10-18 15:24:36 +0100

    Update MaximumSurfacePressureCommandShould.cs

--- BubblesDivePlannerTests/Controllers/DiveStages/MaximumSurfacePressureCommandShould.cs
+++ BubblesDivePlannerTests/Controllers/DiveStages/MaximumSurfacePressureCommandShould.cs
@@ -1,43 +1,28 @@
-// using BubblesDivePlanner.Controllers.DiveStages;
-// using BubblesDivePlanner.DiveStages;
-// using BubblesDivePlanner.Models.DiveModels;
-// using BubblesDivePlannerTests.TestFixtures;
-// using Moq;
-// using Xunit;
+using BubblesDivePlanner.Controllers.DiveStages;
+using BubblesDivePlanner.DiveStages;
+using BubblesDivePlanner.Models.DiveModels;
+using BubblesDivePlannerTests.TestFixtures;
+using Moq;
+using Xunit;
 
-// namespace BubblesDivePlannerTests.DiveStages
-// {
-//     public class MaximumSurfacePressureCommandShould
-//     {
-        
-//         [Fact]
-//         public void RunMaximumSurfacePressureStage()
-//         {
-//             //Arrange
-//             var diveModel = new Zhl16Buhlmann();
-//             diveModel.DiveProfile = ExpectedDiveProfile();
-//             // diveModel.DiveProfile = DivePlannerApplicationTestFixture.GetDiveProfileResultFromFirstRun;
-//             // var aValues = diveModel.DiveProfile.AValues;
-//             // var bValues = diveModel.DiveProfile.BValues;
-//             // diveModel.DiveProfile.AValues = aValues;
-//             // diveModel.DiveProfile.BValues = bValues;
-//             var expectedMaxSurfacePresureResult = diveModel.DiveProfile.MaxSurfacePressures;
+namespace BubblesDivePlannerTests.DiveStages
+{
+    public class MaximumSurfacePressureCommandShould
+    {
+        [Fact]
+        public void RunMaximumSurfacePressureStage()
+        {
+            //Arrange
+            var diveModel = TestFixture.FixtureDiveModel;
+            diveModel.DiveProfile.AValues = TestFixture.ExpectedAValues;
+            diveModel.DiveProfile.BValues = TestFixture.ExpectedBValues;
+            var diveStage = new MaximumSurfacePressure(diveModel);
 
-//             var diveStage = new MaximumSurfacePressureCommand(diveModel);
+            //Act
+            diveStage.RunDiveStage();
 
-//             //Act
-//             diveStage.RunDiveStage();
-
-//             //Assert
-//             Assert.Equal(expectedMaxSurfacePresureResult, diveModel.DiveProfile.MaxSurfacePressures);
-//         }
-
-//         private IDiveProfile ExpectedDiveProfile()
-//         {
-//             var diveProfile = new Mock<IDiveProfile>();
-//             diveProfile.Setup(dp => dp.MaxSurfacePressures).Returns(new double[1]);
-
-//             return diveProfile.Object;
-//         }
-//     }
-// }
+            //Assert
+            Assert.Equal(TestFixture.ExpectedMaxSurfacePressures, diveModel.DiveProfile.MaxSurfacePressures);
+        }
+    }
+}
